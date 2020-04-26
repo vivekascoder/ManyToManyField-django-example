9665aa7a99ac7e9b12db16a4ab6575bb6f9adff7
VivekAsCoder <54495208+vivekascoder@users.noreply.github.com>
2020-04-26 10:21:01 +0530

    indicating a comment

--- learning/forms.py
+++ learning/forms.py
@@ -10,5 +10,6 @@ class PostCreationForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(PostCreationForm, self).__init__(*args, **kwargs)
+        # Below Two lines are particularly important.
         self.fields['tags'].widget = CheckboxSelectMultiple()
         self.fields['tags'].queryset = Tag.objects.all()
